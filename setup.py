a73aebc7ff4e993376cd30330e08fab7d47c5d5f
mosphoxg <mosphox@gmail.com>
2025-06-09 00:50:52 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='keyvomit',
-    version='1.0.0',
+    version='1.0.1',
     description='Unhinged character sequence generator for passwords, tokens, and general chaos.',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
